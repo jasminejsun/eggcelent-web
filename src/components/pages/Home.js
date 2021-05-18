abb3da93170c0833bda1d41a7aabce3ebfe45950
jasminejsun <71673279+jasminejsun@users.noreply.github.com>
2021-05-18 16:55:01 -0400

    Created cards and images

--- src/components/pages/Home.js
+++ src/components/pages/Home.js
@@ -1,11 +1,14 @@
 import React from 'react';
 import '../../App.css';
 import Header from '../Header';
+import Cards from '../Cards';
+
 
 function Home () {
     return (
         <>
             <Header />
+            <Cards />
         </>
     )
 }
