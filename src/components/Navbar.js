1833cf19adb0545a0060d0e7f3a505af3a35f6ae
jasminejsun <71673279+jasminejsun@users.noreply.github.com>
2021-05-18 01:02:25 -0400

    Created responsive navbar

---
+++ src/components/Navbar.js
@@ -0,0 +1,44 @@
+import React, {useState} from 'react';
+import { Link } from 'react-router-dom';
+import '../components/Navbar.css';
+
+function Navbar() {
+    const [click, setClick] = useState(false);
+   
+    const handleClick = () => setClick(!click);
+    const closeMobileMenu = () => setClick(false);
+    return (
+        <>
+            <nav className="navbar">
+                <div className="navbar-container">
+                    <Link to="/" className="navbar-logo">
+                        <span className="logo-font">
+                        eggcelent
+                        </span>
+                    </Link>
+                    <div className='menu-icon' onClick={handleClick}>
+                        <i className={click ? 'fas fa-times' : 'fas fa-bars'} />
+                    </div>
+                    <ul className={click ? 'nav-menu active' : 'nav-menu'}>
+                        <li className='nav-item'>
+                            <Link to='/' className='nav-links' onClick={closeMobileMenu}>
+                                <span className="header-font">
+                                    Home
+                                </span>
+                            </Link>
+                            </li>
+                            <li className='nav-item'>
+                            <Link to='/about' className='nav-links' onClick={closeMobileMenu}>
+                                <span className="header-font">
+                                    About
+                                </span>
+                            </Link>
+                        </li>
+                    </ul>
+                </div>
+            </nav>
+        </>
+    )
+}
+
+export default Navbar
