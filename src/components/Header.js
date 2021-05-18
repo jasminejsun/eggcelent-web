9a2dae318b3d2c8804eb7484976a6721269de464
jasminejsun <71673279+jasminejsun@users.noreply.github.com>
2021-05-18 15:25:41 -0400

    Created header section

---
+++ src/components/Header.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import '../App.css';
+import './Header.css';
+
+function Header() {
+    return (
+        <div className='header-container'>
+            <img src="/images/header-image.png" alt="How do you like your eggs?"/>
+        </div>
+    )
+}
+
+export default Header
